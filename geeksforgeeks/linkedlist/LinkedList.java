064250bb65adc560a3ebc7ee0f0c170d441c14d3
Tejaswini Patil <tejaswini.patil030@gmail.com>
2020-06-09 13:18:05 +0530

    Reverse linked list recursive and iterative solution

--- geeksforgeeks/linkedlist/LinkedList.java
+++ geeksforgeeks/linkedlist/LinkedList.java
@@ -12,7 +12,42 @@ class Node
 class List
 {
     Node head;
+    Node reverseLLrecursive(Node head)
+    {
+        // base case
+        if( head == null || head.next == null)
+        {
+            return head;
+        }
+        // get head of reversed list excluding head
+        Node rhead = reverseLLrecursive(head.next);
+        // add head into reversed list
+        head.next.next = head;
+        head.next = null;
+        return rhead;
+    }
     
+    // iterative solution to reverse a LL 
+    Node reverseLLiterative(Node head)
+    {   
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+
+        if(head == null || head.next == null)
+        {
+            return head;
+        }
+
+        while(curr != null )
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
     void addAtEnd(int data)
     {
         Node temp = new Node(data);
@@ -46,6 +81,17 @@ public class LinkedList
         l.addAtEnd(10);
         l.addAtEnd(20);
         l.addAtEnd(30);
+        l.addAtEnd(40);
+        l.addAtEnd(50);
+        l.addAtEnd(60);
+        l.display();
+
+        // reverse linked list iterative solution 
+
+        l.head = l.reverseLLiterative(l.head);
+        l.display();
+
+        l.head = l.reverseLLrecursive(l.head);
         l.display();
     }
 }

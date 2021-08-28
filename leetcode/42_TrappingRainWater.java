5b5fc5076fd92cc1e1050628f01858eba69e38f2
Tejaswini Patil <tejaswini.patil030@gmail.com>
2021-08-27 22:49:27 -0700

    Day 13 Leetcode

---
+++ leetcode/42_TrappingRainWater.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int []left_max = new int[n];
+        int []right_max = new int[n];
+        
+        left_max[0] = height[0];
+        for(int i = 1; i< n ;i++)
+        {
+            left_max[i] =  Math.max(left_max[i-1], height[i]);
+        }
+        
+        right_max[n-1] = height [n-1];
+        for(int i = n-2; i>=0; i--)
+        {
+            right_max[i] = Math.max( right_max[i+1], height[i]);
+        }
+        
+        int ans = 0;
+        for(int i =0; i<n; i++)
+        {
+            ans += Math.min(left_max[i], right_max[i]) - height[i];
+        }
+        return ans;
+    }
+}

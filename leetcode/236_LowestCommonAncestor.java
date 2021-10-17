ef837d7ade2f9d065c0cc310c6e3a9f1e0057e24
Tejaswini Patil <tejaswini.patil030@gmail.com>
2021-10-17 00:26:59 -0700

    Day 35 LeetCode

--- leetcode/236_LowestCommonAncestor.java
+++ leetcode/236_LowestCommonAncestor.java
@@ -7,6 +7,55 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// Iterative solution 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Stack<TreeNode> stack = new Stack<>();
+        
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        
+        parent.put(root, null);
+        
+        stack.push(root);
+        while(!parent.containsKey(p) || !parent.containsKey(q)){
+            TreeNode curr = stack.pop();
+            
+            if(curr.left != null)
+            {
+                parent.put(curr.left, curr);
+                stack.push(curr.left);
+            }
+            if(curr.right != null ){
+                parent.put(curr.right, curr);
+                stack.push(curr.right);
+            }
+            
+        }
+        
+        Set<TreeNode> set = new HashSet<TreeNode>();
+        while(p!= null){
+            set.add(p);
+            p = parent.get(p);
+        }
+        
+        while(!set.contains(q)){
+            q = parent.get(q);
+        }
+        return q;
+    }
+}
+
+// recursive
+
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null ) return null;

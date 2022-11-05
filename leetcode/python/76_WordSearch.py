b4112c8c8b2742a3a523875b6d03e45068d461d7
Tejaswini Patil <tejaswini.patil030@gmail.com>
2022-11-05 06:03:37 -0700

    pyday 5

---
+++ leetcode/python/76_WordSearch.py
@@ -0,0 +1,39 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+
+        dirs = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+        visited = set()
+
+        def dfs(start, ptr):
+            visited.add(start)
+            if ptr >= len(word):
+                return True
+
+            for x, y in dirs:
+                tx = start[0] + x
+                ty = start[1] + y
+                # out of bounds
+                if tx < 0 or ty < 0 or tx >= m or ty >= n:
+                    continue
+                
+                if board[tx][ty] == word[ptr] and (tx, ty) not in visited:
+                    tmp = dfs((tx, ty), ptr + 1)
+                    if tmp:
+                        return True
+                    # print(visited)
+                    visited.remove((tx, ty))
+
+            
+            return False
+
+        ptr = 0
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[ptr]:
+                    tmp = dfs((i, j), ptr+1)
+                    if tmp:
+                        return True
+                    visited.remove((i,j))
+        return False

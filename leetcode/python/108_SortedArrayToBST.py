10c63b3d2959e5772a3f79b7d1a17da1ff1c480f
Tejaswini Patil <tejaswini.patil030@gmail.com>
2022-11-12 16:32:07 -0800

    pyday 9

---
+++ leetcode/python/108_SortedArrayToBST.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
+    
+        def constructBST(start, end):
+            if start > end:
+                return None
+            mid = start + (end - start)//2
+
+            node = TreeNode(nums[mid])
+            node.left = constructBST(start, mid - 1)
+            node.right = constructBST(mid + 1, end)
+
+            return node
+        
+        return constructBST(0, len(nums) - 1)
